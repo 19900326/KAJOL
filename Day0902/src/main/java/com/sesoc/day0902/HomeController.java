8f733daa20ff2cb1b554983b844a65239011690b
polalone <tosanawai@gmail.com>
2020-10-16 21:47:22 +0900

    MH10181

--- Day0902/src/main/java/com/sesoc/day0902/HomeController.java
+++ Day0902/src/main/java/com/sesoc/day0902/HomeController.java
@@ -93,6 +93,14 @@ public class HomeController {
 		return "Mypage";
 	}
 
+	@RequestMapping(value = "/loginInsert", method = RequestMethod.GET)
+	public String loginInsert() {
+		return "loginInsert";
+	}
 	
+	@RequestMapping(value = "/signUp", method = RequestMethod.GET)
+	public String signUp() {
+		return "signUp";
+	}
 
 }
